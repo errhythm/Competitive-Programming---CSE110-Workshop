c4eb4b981d8475018bf9e6a3485df9417968b55b
Ehsanur Rahman Rhythm <40324252+errhythm@users.noreply.github.com>
2020-02-15 21:06:38 +0600

    UVA 10055
    Origin: UVA 10055

---
+++ F.java
@@ -0,0 +1,17 @@
+// Author: Sarin Rahman Zavin
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String[] args) 
+{
+        Scanner inp=new Scanner(System.in);
+        while(inp.hasNextLong())
+{
+            long hasmit=inp.nextLong();
+            long opp=inp.nextLong();
+            long diff=Math.abs(opp-hasmit);
+            System.out.println(diff);
+        }
+    }
+}
