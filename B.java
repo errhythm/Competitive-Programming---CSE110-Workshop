e0c31f773f7bca30aad085ff25b62cd48b155c7e
Ehsanur Rahman Rhythm <40324252+errhythm@users.noreply.github.com>
2020-02-15 20:43:04 +0600

    CodeForces 630A
    Origin: CodeForces 630A

---
+++ B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+  
+  public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        long n1 = in.nextLong();
+        long diff = 25;
+               
+        System.out.println(diff); 
+  }
+}
